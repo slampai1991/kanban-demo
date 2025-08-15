71b6ff9e44e0b91a203f837ff2d5995b0191027b
slampai1991 <btdwnhc@gmail.com>
2025-08-15 16:27:27 +0300

    Update task-dialog.component.ts

--- kanban-frontend/src/app/task-dialog/task-dialog.component.ts
+++ kanban-frontend/src/app/task-dialog/task-dialog.component.ts
@@ -1,10 +1,76 @@
+// import { Component, OnInit, Inject } from '@angular/core';
+// import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material';
+// import { Task } from '../model/task/task';
+// import { MatInputModule } from '@angular/material/input';
+// import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+// import { KanbanService } from '../service/kanban-service.service';
+// import { TaskService } from '../service/task.service';
+
+// @Component({
+//   selector: 'app-task-dialog',
+//   templateUrl: './task-dialog.component.html',
+//   styleUrls: ['./task-dialog.component.css']
+// })
+// export class TaskDialogComponent implements OnInit {
+
+//   dialogTitle: String;
+//   kanbanId: String;
+//   task: Task;
+
+//   form: FormGroup;
+
+//   constructor(
+//     private fb: FormBuilder,
+//     private dialogRef: MatDialogRef<TaskDialogComponent>,
+//     @Inject(MAT_DIALOG_DATA) data,
+//     private kanbanService: KanbanService,
+//     private taskService: TaskService) {
+
+//     this.dialogTitle = data.title;
+//     this.kanbanId = data.kanbanId;
+//     this.task = data.task;
+
+//     this.form = fb.group({
+//       title: [this.task.title, Validators.required],
+//       description: [this.task.description, Validators.required],
+//       color: [this.task.color,Validators.required]
+//   });
+//    }
+
+//   ngOnInit() {
+//   }
+
+//   save() {
+//     this.mapFormToTaskModel();
+//     if (!this.task.id) {
+//       this.kanbanService.saveNewTaskInKanban(this.kanbanId, this.task).subscribe();
+//     } else {
+//       this.taskService.updateTask(this.task).subscribe();
+//     }
+//     this.dialogRef.close();
+//     window.location.reload();
+//   }
+
+//   close() {
+//       this.dialogRef.close();
+//   } 
+
+//   private mapFormToTaskModel(): void {
+//     this.task.title = this.form.get('title').value;
+//     this.task.description = this.form.get('description').value;
+//     this.task.color = this.form.get('color').value;
+//     this.task.status = 'TODO';
+//   }
+
+// }
+
+// task-dialog.component.ts
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialog } from '@angular/material';
-import { Task } from '../model/task/task';
-import { MatInputModule } from '@angular/material/input';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { KanbanService } from '../service/kanban-service.service';
 import { TaskService } from '../service/task.service';
+import { Task } from '../model/task/task';
 
 @Component({
   selector: 'app-task-dialog',
@@ -13,10 +79,9 @@ import { TaskService } from '../service/task.service';
 })
 export class TaskDialogComponent implements OnInit {
 
-  dialogTitle: String;
-  kanbanId: String;
+  dialogTitle: string;
+  kanbanId: string;
   task: Task;
-
   form: FormGroup;
 
   constructor(
@@ -24,8 +89,8 @@ export class TaskDialogComponent implements OnInit {
     private dialogRef: MatDialogRef<TaskDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
     private kanbanService: KanbanService,
-    private taskService: TaskService) {
-
+    private taskService: TaskService
+  ) {
     this.dialogTitle = data.title;
     this.kanbanId = data.kanbanId;
     this.task = data.task;
@@ -33,12 +98,11 @@ export class TaskDialogComponent implements OnInit {
     this.form = fb.group({
       title: [this.task.title, Validators.required],
       description: [this.task.description, Validators.required],
-      color: [this.task.color,Validators.required]
-  });
-   }
-
-  ngOnInit() {
+      color: [this.task.color, Validators.required]
+    });
   }
+
+  ngOnInit() {}
 
   save() {
     this.mapFormToTaskModel();
@@ -48,12 +112,11 @@ export class TaskDialogComponent implements OnInit {
       this.taskService.updateTask(this.task).subscribe();
     }
     this.dialogRef.close();
-    window.location.reload();
   }
 
   close() {
-      this.dialogRef.close();
-  } 
+    this.dialogRef.close();
+  }
 
   private mapFormToTaskModel(): void {
     this.task.title = this.form.get('title').value;
@@ -61,5 +124,4 @@ export class TaskDialogComponent implements OnInit {
     this.task.color = this.form.get('color').value;
     this.task.status = 'TODO';
   }
-
 }
